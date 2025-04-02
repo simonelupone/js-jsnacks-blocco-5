73c2d9f280ebce073357c2ee3745dc654a69efdb
simonelupone <simone.lupone@gmail.com>
2025-04-02 14:38:44 +0200

    [snack 4]: complete

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorArray = posts.map(post => post.author)
+console.log(authorArray)
